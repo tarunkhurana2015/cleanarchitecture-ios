c439ebb93ea52ebdf6a5f149eaeb101e0ea2be17
Tarun Khurana <tarunkhurana1982@gmail.com>
2024-06-25 21:42:02 -0500

    Added the new video link

--- CleanArchitecture/Video/Sources/Data/VideoAPI/VideoAPI.swift
+++ CleanArchitecture/Video/Sources/Data/VideoAPI/VideoAPI.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 // https://gist.githubusercontent.com/taruncopper/4f189111ec97d91b1fbbefd32904585e/raw/440d30aae9b92b92538276b688805d884e1c36ab/gistfile1.txt
+// https://gist.githubusercontent.com/tarunkhurana2015/b403e8df087bce1f8459f59c412a7cc1/raw/551d06446c66849e827cbaa1d41c0a69ae7b4bd1/gistfile1.txt
+
 enum VideoAPI {
     static let baseUrl = "https://gist.githubusercontent.com"
-    static let basePath = "taruncopper/4f189111ec97d91b1fbbefd32904585e/raw"
-    static let apiKey = "440d30aae9b92b92538276b688805d884e1c36ab"
+    static let basePath = "tarunkhurana2015/b403e8df087bce1f8459f59c412a7cc1/raw"
+    static let apiKey = "551d06446c66849e827cbaa1d41c0a69ae7b4bd1"
     static let moviesFile = "gistfile1.txt"
     
     static let imageBaseUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample"
